77237ecf6a6bac4c85ee35ed0ae2d4231a7c858f
梅林的法杖 <530264127@qq.com>
2021-03-06 00:01:16 +0800

    xx

--- .umi-production/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from '/Users/wuxiao/wuxiao-desktop/weisuoke/doc/awesome-fe/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['patchRoutes','rootContainer','render','onRouteChange','ssr',],
-});
-
-export { plugin };
