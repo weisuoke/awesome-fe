4ff85dc681ffeca8f96e913d126122e778351dd4
weisuoke <530264127@qq.com>
2023-01-05 21:36:30 +0800

    release_20230105

---
+++ .umi-production/core/plugin.ts
@@ -0,0 +1,8 @@
+// @ts-nocheck
+import { Plugin } from '/Users/wuxiao/Documents/wuxiao.io/doc/awesome-fe/node_modules/@umijs/runtime';
+
+const plugin = new Plugin({
+  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','__mfsu','ssr',],
+});
+
+export { plugin };
